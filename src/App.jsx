9311a1714a30768823397e7c1dcacfba96e0bf47
Amisha Choudhary <amishachoudhary@Amishas-MacBook-Air.local>
2025-03-05 19:05:22 +0530

    Updated portfolio with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./sections/Navbar.jsx"
 import Hero from "./sections/Hero.jsx"
 import About from './sections/About.jsx'
 import Projects from './sections/Projects.jsx'
-import Clients from './sections/clients.jsx'
+import Clients from './sections/Clients.jsx'
 import Footer from './sections/Footer.jsx'
 import Contact from './sections/Contact.jsx'
 import WorkExperience from './sections/Experience.jsx'
